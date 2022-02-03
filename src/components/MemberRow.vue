1cc295d0c627866a53df1ce0be27e95e39756ec3
cb-shivam <98250333+cb-shivam@users.noreply.github.com>
2022-02-03 16:51:11 +0530

    Added Component for displaying each row

---
+++ src/components/MemberRow.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="row text-start justify-content-center mt-0">
+    <div class="col-1 border border-light border-end-0 border-3">
+      <img src="@/assets/dp_placeholder.png" class="rounded-start img-thumbnail mt-1 mb-1"
+           alt="..." width="70%">
+    </div>
+    <div class="col-5 border-light border-top border-bottom border-3">
+      <p class="mt-2">You<br>you@chargebee.com</p>
+    </div>
+
+    <div class="col-1 border border-light border-start-0 border-3">
+      <div class="mt-3">
+        <img src="@/assets/three-dots-vertical.svg" alt="Bootstrap">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MemberRow',
+};
+</script>
